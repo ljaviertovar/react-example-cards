841bd570bcac8997feca017c647df95c88970495
ljaviertovar <luisjavier_tovar@outlook.com>
2021-05-15 13:23:12 -0500

    :lipstick: [ADD] styels

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './cards.css';
+
 const Card = ({ imageSource, title, url, text }) => {
     return (
-        <div className="card text-center bg-dark">
-            <img src={imageSource} alt="" />
+        <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+            <div className="div-img">
+                <img src={imageSource} className="card-img-top" alt="" />
+            </div>
             <div className="card-body text-light">
                 <h4 className="card-title">{title}</h4>
                 <p className="card-text text-secondary">
                     {text
-                    ? text
-                    : 'Eu quis tempor qui officia occaecat sit cillum consequat labore. Sunt nostrud fugiat quis reprehenderit eiusmod. Quis culpa culpa velit sit do ullamco. Amet est nisi culpa tempor deserunt exercitation duis. Excepteur ex minim laborum velit consectetur culpa aliquip.'}
-            </p>
+                        ? text
+                        : 'Eu quis tempor qui officia occaecat sit cillum consequat labore. Sunt nostrud fugiat quis reprehenderit eiusmod. Quis culpa culpa velit sit do ullamco. Amet est nisi culpa tempor deserunt exercitation duis. Excepteur ex minim laborum velit consectetur culpa aliquip.'}
+                </p>
                 <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank">
                     Go to this website
             </a>
